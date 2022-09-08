815f778dd62d6458941c76724d34af43d4c80499
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-09-08 20:08:22 +0700

    Widget Builder

--- lib/main.dart
+++ lib/main.dart
@@ -11,19 +11,6 @@ void main() {
 class MyApp extends StatelessWidget {
   MyApp({super.key});
 
-  List<Map<String, dynamic>> data = List.generate(
-    10,
-    (index) => {
-      "text": "Kotak ${index + 1}",
-      "color": Color.fromARGB(
-        255,
-        Random().nextInt(256),
-        Random().nextInt(256),
-        Random().nextInt(256),
-      ),
-    },
-  );
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -31,16 +18,16 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text("Reusable Widget"),
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: data
-                .map(
-                  (e) => KotakWarna(
-                    text: e["text"],
-                    color: e["color"],
-                  ),
-                )
-                .toList(),
+        body: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (context, index) => KotakWarna(
+            text: "Kotak ke-${index + 1}",
+            color: Color.fromARGB(
+              255,
+              150 + Random().nextInt(256),
+              150 + Random().nextInt(256),
+              150 + Random().nextInt(256),
+            ),
           ),
         ),
       ),
