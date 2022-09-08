ea02931becb9cf5a770f4bc416de737c488b6eb7
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-09-08 21:14:42 +0700

    Latihan 4

---
+++ lib/widgets/latihan_4.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class Latihan4 extends StatelessWidget {
+  const Latihan4({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amber[200],
+        leading: const FlutterLogo(),
+        title: const Text('Latihan 3'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.more_vert),
+          )
+        ],
+      ),
+      body: Row(
+        children: [
+          Container(
+            width: 150,
+            height: 150,
+            color: Colors.blue[800],
+            child: const Center(
+              child: Text(
+                "Hello",
+                style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(width: 20),
+          Container(
+            width: 150,
+            height: 150,
+            color: Colors.amber,
+            child: const Center(
+              child: Text(
+                "Hello",
+                style: TextStyle(fontSize: 25),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
