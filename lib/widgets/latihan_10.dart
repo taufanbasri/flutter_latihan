1a3bcabba28b76eabd3ce03d3c8e854d327af201
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-09-09 11:47:46 +0700

    Latihan 10: Stack +

--- lib/widgets/latihan_10.dart
+++ lib/widgets/latihan_10.dart
@@ -20,21 +20,48 @@ class Latihan10 extends StatelessWidget {
         ],
       ),
       body: Center(
-        child: Container(
-          width: 250,
-          height: 250,
-          decoration: BoxDecoration(
-            border: Border.all(
-              color: Colors.blue,
-              width: 10,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                ClipOval(
+                  child: Container(
+                    height: 275,
+                    width: 275,
+                    color: Colors.blue[900],
+                  ),
+                ),
+                Container(
+                  width: 250,
+                  height: 250,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.white,
+                      width: 10,
+                    ),
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.circular(250 / 2),
+                    image: const DecorationImage(
+                      fit: BoxFit.cover,
+                      image: NetworkImage("https://picsum.photos/500/500"),
+                    ),
+                  ),
+                ),
+              ],
             ),
-            color: Colors.grey[300],
-            borderRadius: BorderRadius.circular(250 / 2),
-            image: const DecorationImage(
-              fit: BoxFit.cover,
-              image: NetworkImage("https://picsum.photos/500/500"),
+            const SizedBox(height: 50),
+            const Text(
+              "Hello World!",
+              style: TextStyle(
+                fontSize: 35,
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.bold,
+                decoration: TextDecoration.underline,
+              ),
             ),
-          ),
+          ],
         ),
       ),
     );
