5991402ef22897b7598c806fc67f4b4f745ec01c
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-09-09 10:13:22 +0700

    Latihan 8: BoxDecoration

--- lib/widgets/latihan_8.dart
+++ lib/widgets/latihan_8.dart
@@ -21,17 +21,9 @@ class Latihan8 extends StatelessWidget {
       ),
       body: ListView.builder(
         padding: const EdgeInsets.all(20),
-        itemCount: 50,
+        itemCount: 10,
         itemBuilder: (BuildContext context, int index) {
-          if (index % 2 == 0) {
-            return KotakWarna(
-              color: Colors.blue,
-              index: index + 1,
-            );
-          }
-
           return KotakWarna(
-            color: Colors.amber,
             index: index + 1,
           );
         },
@@ -43,29 +35,34 @@ class Latihan8 extends StatelessWidget {
 class KotakWarna extends StatelessWidget {
   const KotakWarna({
     Key? key,
-    required this.color,
     required this.index,
   }) : super(key: key);
 
-  final Color color;
   final int index;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(bottom: 20),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            height: 150,
-            color: color,
+      child: Container(
+        padding: const EdgeInsets.all(20),
+        height: 200,
+        decoration: BoxDecoration(
+          color: Colors.grey[300],
+          borderRadius: BorderRadius.circular(20),
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: NetworkImage("https://picsum.photos/id/${778 + index}/200/300"),
           ),
-          Text(
-            "Hello $index",
-            style: const TextStyle(fontSize: 25),
+        ),
+        alignment: Alignment.bottomLeft,
+        child: Text(
+          "Hello $index",
+          style: const TextStyle(
+            fontSize: 25,
+            color: Colors.white,
           ),
-        ],
+        ),
       ),
     );
   }
